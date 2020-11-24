d396b366e8d9cf76976dc29ff1bbe8b76b182211
LAPTOP-B32DH16E\XUBXUE <2370237499@qq.com>
2020-11-24 16:09:57 +0800

    使用sass的for循环，实现电量格

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -13,14 +13,14 @@
           <div>
             <h4 class="car-number">京 A6666</h4>
             <div>
-              <ul class="car-electric">
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
-                <li class="active"></li>
+              <ul class="car-electric active-li-7">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
                 <li></li>
                 <li></li>
                 <li></li>
@@ -35,7 +35,9 @@
         </div>
         <img src="../../assets/images/pic001.jpg" alt="">
       </div>
-      <footer></footer>
+      <footer>
+        <a href="javascript: void(0);" class="parking-link">某某停车场</a>
+      </footer>
     </section>
     <div>汽车列表</div>
   </div>
@@ -65,7 +67,7 @@ export default {
   width: 505px;
   height: 257px;
   margin: auto;
-  padding: 30px;
+  padding: 30px 30px 0;
   border-radius: 24px;
   background-color: #FFF;
   @include webkit(box-shadow, 0 0 18px 0 rgba(0, 0, 0, .2));
@@ -118,9 +120,14 @@ export default {
       background-color: #e1e1e1;
       border-radius: 100px;
     }
-    li.active {
-      @include webkitA(background, linear-gradient(#17a8fa, #108dd9));
+    @for $i from 1 through 10 {
+      &.active-li-#{$i} {
+        li:nth-child(-n+#{$i}) { @include webkitA(background, linear-gradient(#17a8fa, #108dd9)); }
+      }
     }
+    // li.active {
+    //   @include webkitA(background, linear-gradient(#17a8fa, #108dd9));
+    // }
   }
   .car-distance {
     margin-left: 5px;
@@ -136,6 +143,26 @@ export default {
     sub:first-of-type {
       opacity: 0.8;
     }
+  }
+}
+.parking-link {
+  position: relative;
+  display: block;
+  border-top: 1px solid #e1e1e1;
+  color: $main-color;
+  opacity: .8;
+  line-height: 53px;
+  &::after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 50%;
+    width: 8px;
+    height: 8px;
+    margin-top: -8px;
+    border-top: 1px solid #d6d7d9;
+    border-right: 1px solid #d6d7d9;
+    @include webkit(transform, rotate(45deg));
   }
 }
 </style>
