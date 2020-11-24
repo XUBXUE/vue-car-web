90a71dc0dda171902efeac49968657edf98a86f6
LAPTOP-B32DH16E\XUBXUE <2370237499@qq.com>
2020-11-24 15:47:35 +0800

    添加汽车卡片 footer部分 电源格

--- src/views/cars/index.vue
+++ src/views/cars/index.vue
@@ -8,7 +8,33 @@
         </h4>
         <p class="car-attr">新能源汽车  5座</p>
       </header>
-      <div class="car-content"></div>
+      <div class="car-content">
+        <div class="info">
+          <div>
+            <h4 class="car-number">京 A6666</h4>
+            <div>
+              <ul class="car-electric">
+                <li class="active"></li>
+                <li class="active"></li>
+                <li class="active"></li>
+                <li class="active"></li>
+                <li class="active"></li>
+                <li class="active"></li>
+                <li class="active"></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+              <p class="car-distance">
+                <sub>约</sub>
+                <strong>600</strong>
+                <sub>KM</sub>
+              </p>
+            </div>
+          </div>
+        </div>
+        <img src="../../assets/images/pic001.jpg" alt="">
+      </div>
       <footer></footer>
     </section>
     <div>汽车列表</div>
@@ -60,5 +86,56 @@ export default {
   float: right;
   line-height: 34px;
   opacity: .5;
+}
+.car-content {
+  position: relative;
+  margin: 11px 0 7px;
+  @extend %clearfix;
+  img {
+    float: right;
+    height: 120px;
+  }
+  .info {
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+  }
+  .car-number {
+    font-size: 18px;
+  }
+  .car-electric {
+    padding-top: 10px;
+    display: inline-block;
+    li {
+      display: inline-block;
+      width: 7px;
+      height: 24px;
+      margin-right: 3px;
+      background-color: #e1e1e1;
+      border-radius: 100px;
+    }
+    li.active {
+      @include webkitA(background, linear-gradient(#17a8fa, #108dd9));
+    }
+  }
+  .car-distance {
+    margin-left: 5px;
+    display: inline-block;
+    strong {
+      font-size: 22px;
+      margin: 0 5px;
+    }
+    sub {
+      position: relative;
+      top: -2px;
+    }
+    sub:first-of-type {
+      opacity: 0.8;
+    }
+  }
 }
 </style>
